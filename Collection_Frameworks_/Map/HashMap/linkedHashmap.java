68e0b5df775631bb75725b4865c9c3493fc08fb5
Abhinav Raj Srivastava <abhiraj.srivast254@gmail.com>
2023-09-27 22:22:43 +0530

    27-09-2023-002

---
+++ Collection_Frameworks_/Map/HashMap/linkedHashmap.java
@@ -0,0 +1,50 @@
+package Map.HashMap;
+
+import java.util.*;
+
+public class linkedHashmap {
+    public static void main(String[] args){
+        //collection of key-value pairs is called an entry...
+        LinkedHashMap<Integer, String> lhm = new LinkedHashMap<>();
+
+        //Adding Key and Value pairs to LinkedHashMap...
+        lhm.put(1,"Abdul");
+        lhm.put(4,"Abhinav");
+        lhm.put(5,"Abhishek");
+
+        //Printing the LinkedHashMap (ordered)...
+        System.out.println("LinkedHashMap: " + lhm);
+
+        //Getting a value for a given key...
+        String name = lhm.get(4);
+        System.out.println("Name of Roll no. 4 is: " + name);
+
+        //Checking if a Key exists...
+        System.out.println("Contains key 5? " + lhm.containsKey(5));
+
+        //Checking if a Value exists...
+        System.out.println("contains value 'Abdul'? " + lhm.containsValue("Abdul"));
+
+        //Removing a Key...
+        String name2 = lhm.remove(5);
+        System.out.println("Removed value: " + name2);
+        System.out.println("Updated LinkedHashMap: " + lhm);
+
+        //Size of HashMap...
+        System.out.println("Size: " + lhm.size());
+
+        //Checking if HashMap is empty...
+        System.out.println("Is empty? " + lhm.isEmpty());
+
+        //Getting all the keys present in the HashMap...
+        System.out.println("KeySet: " + lhm.keySet());
+
+        //Getting all the values present in the HashMap...
+        System.out.println("Values: " + lhm.values());
+
+        //clearing the HashMap...
+        lhm.clear();
+
+    }
+
+}
