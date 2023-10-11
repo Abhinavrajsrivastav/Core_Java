28df0c3772891ec04cec552718c588c2c4e4e04b
Abhinav Raj Srivastava <abhiraj.srivast254@gmail.com>
2023-10-12 00:15:50 +0530

    12-10-2023-001

--- Multiple_threading/Multi_threading1.java
+++ Multiple_threading/Multi_threading1.java
@@ -1,8 +1,15 @@
 package Multiple_threading;
+
+import java.util.Scanner;
+
 // Create a thread by implementing Runnable interface
 class Thread1 implements Runnable {
     public void run() {
        System.out.println("Thread1 is running...");
+       Scanner sc = new Scanner(System.in);
+         System.out.println("Enter a number: ");
+            int n = sc.nextInt();
+            System.out.println("The number is: " + n+n);
     }
 } 
 
@@ -10,8 +17,21 @@ class Thread1 implements Runnable {
 class Thread2 implements Runnable {
     public void run() {
        System.out.println("Thread2 is running...");
+       Scanner sc = new Scanner(System.in);
+         System.out.println("Enter a number: ");
+            int n = sc.nextInt();
+            System.out.println("The number is: " +n);
     }
-}   
+}  
+class Thread3 implements Runnable {
+    public void run() {
+       System.out.println("Thread3 is running...");
+       Scanner sc = new Scanner(System.in);
+         System.out.println("Enter a number: ");
+            int n = sc.nextInt();
+            System.out.println("The number is: " + n*n);
+    }
+}  
 
 // Main class
 public class Multi_threading1 {
@@ -19,8 +39,10 @@ public class Multi_threading1 {
         // Create two threads
         Thread t1 = new Thread(new Thread1());
         Thread t2 = new Thread(new Thread2());
+        Thread t3 = new Thread(new Thread3());
         // Start the threads
         t1.start();
         t2.start();
+        t3.start();
     }
 }
