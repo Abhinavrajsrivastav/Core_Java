aae6bc88333e9650883bcaf509faca9382e593a4
Abhinav Raj Srivastava <abhiraj.srivast254@gmail.com>
2023-10-19 11:50:38 +0530

    19-10-2023-001

---
+++ Algorithms/Backtracking.java
@@ -0,0 +1,30 @@
+package Algorithms;
+
+public class Backtracking {
+
+    public static void backtracking(int[] arr, int[] data, int index, int i, int n, int r) {
+        if (index == r) {
+            for (int j = 0; j < r; j++) {
+                System.out.print(data[j] + " ");
+            }
+            System.out.println();
+            return;
+        }
+        if (i >= n) {
+            return;
+        }
+        data[index] = arr[i];
+        backtracking(arr, data, index + 1, i + 1, n, r);
+        backtracking(arr, data, index, i + 1, n, r);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        int[] data = new int[3];
+        int n = arr.length;
+        int r = 2;
+        int index = 0;
+        int i = 0;
+        backtracking(arr, data, index, i, n, r);
+    }
+}
